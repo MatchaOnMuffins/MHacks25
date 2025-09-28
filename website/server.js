cc5cccda67833c52c937ed383db6dfd24700fb14
Henry Zhang <zeri@umich.edu>
2025-09-28 04:55:53 -0400

    Add node backend server

--- website/server.js
+++ website/server.js
@@ -43,5 +43,5 @@ app.get("/api/feedback", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
